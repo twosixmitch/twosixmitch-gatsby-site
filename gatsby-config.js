e8edd0ae46f3150d4f62ed20659eaa526a12469d
Mitchell Duncan <twosixmitch@gmail.com>
2021-10-06 10:42:50 +1300

    Adding basics

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Mitchell Duncan',
+    author: { name: 'Mitchell Duncan' },
+    description: 'Mitchell Duncan\'s Website and Blog',
+    siteUrl: "https://twosixmitch.github.io/",
+    pathPrefix: "/twosixmitch-site-wip",
+  },
+  plugins: [],
+};
